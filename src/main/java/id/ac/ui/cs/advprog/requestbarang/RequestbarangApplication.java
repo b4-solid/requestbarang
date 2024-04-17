a8b101e6a6a79fea05843e9dff6bb07796950395
aldenluthfi <alden.luthfi@ui.ac.id>
2024-04-17 19:47:15 +0700

    First Commit!

---
+++ src/main/java/id/ac/ui/cs/advprog/requestbarang/RequestbarangApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.requestbarang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestbarangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestbarangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/requestbarang/RequestbarangApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.requestbarang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestbarangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
