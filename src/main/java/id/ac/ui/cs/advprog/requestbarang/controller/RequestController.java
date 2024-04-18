456a4f422aa5e42aa188031cbe505ca7e7603d32
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-04-18 22:34:23 +0700

    created request controller

---
+++ src/main/java/id/ac/ui/cs/advprog/requestbarang/controller/RequestController.java
@@ -0,0 +1,20 @@
+package id.ac.ui.cs.advprog.requestbarang.controller;
+
+import id.ac.ui.cs.advprog.requestbarang.model.Request;
+import id.ac.ui.cs.advprog.requestbarang.service.RequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/requests")
+public class RequestController {
+
+    @Autowired
+    private RequestService requestService;
+
+    @PostMapping("/create")
+    public Request createRequest(@RequestParam String name, @RequestParam String imageLink, @RequestParam double price,
+            @RequestParam String storeLink) {
+        return requestService.createAndProcessRequest(name, imageLink, price, storeLink);
+    }
+}
