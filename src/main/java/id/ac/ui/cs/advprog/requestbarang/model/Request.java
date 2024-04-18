1585bb3370be1c0675a41d08e15d82efa4546071
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-04-18 22:21:34 +0700

    create request model

---
+++ src/main/java/id/ac/ui/cs/advprog/requestbarang/model/Request.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.requestbarang.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Request {
+    private String name;
+    private String imageLink;
+    private double price;
+    private String storeLink;
+}
