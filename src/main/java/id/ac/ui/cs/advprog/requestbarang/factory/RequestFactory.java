a80d93ed70f0202d5ffd26353043e075e003ff9a
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-04-18 22:29:42 +0700

    create RequestFactory to implement simple factory design pattern

---
+++ src/main/java/id/ac/ui/cs/advprog/requestbarang/factory/RequestFactory.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advprog.requestbarang.factory;
+
+import id.ac.ui.cs.advprog.requestbarang.model.Request;
+
+public class RequestFactory {
+    public static Request createRequest(String name, String imageLink, double price, String storeLink) {
+        return new Request(name, imageLink, price, storeLink);
+    }
+}
