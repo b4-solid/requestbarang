520ab83c36a9769015ee14b13613b9674dd8f4ce
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-04-24 18:01:52 +0700

    add currency variable

--- src/main/java/id/ac/ui/cs/advprog/requestbarang/model/Request.java
+++ src/main/java/id/ac/ui/cs/advprog/requestbarang/model/Request.java
@@ -2,13 +2,22 @@ package id.ac.ui.cs.advprog.requestbarang.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Builder;
 
+@Builder
 @Getter
-@Setter
 public class Request {
     private String name;
     private String imageLink;
     private double price;
+    private String currency;
     private String storeLink;
+    @Setter
     private String status;
+
+    public Request(String name, String imageLink, double price, String currency, String storeLink) {
+    }
+
+    public Request(String name, String imageLink, double price, String currency, String storeLink, String status) {
+    }
 }
