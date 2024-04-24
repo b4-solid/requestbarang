7646add0f56006efb07d1b895806af1467016cca
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-04-24 19:43:04 +0700

    [GREEN] Implement Request model

--- src/main/java/id/ac/ui/cs/advprog/requestbarang/model/Request.java
+++ src/main/java/id/ac/ui/cs/advprog/requestbarang/model/Request.java
@@ -2,6 +2,9 @@ package id.ac.ui.cs.advprog.requestbarang.model;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Arrays;
+
 import lombok.Builder;
 
 @Builder
@@ -17,9 +20,30 @@ public class Request {
     private String status;
 
     public Request(String id, String name, String imageLink, double price, String currency, String storeLink) {
+        this.id = id;
+        this.name = name;
+        this.imageLink = imageLink;
+        this.price = price;
+        this.currency = currency;
+        this.storeLink = storeLink;
+        this.status = "Requesting";
     }
 
     public Request(String id, String name, String imageLink, double price, String currency, String storeLink,
             String status) {
+        this.id = id;
+        this.name = name;
+        this.imageLink = imageLink;
+        this.price = price;
+        this.currency = currency;
+        this.storeLink = storeLink;
+
+        String[] statusList = { "Requesting", "On Sale" };
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
+
     }
 }
