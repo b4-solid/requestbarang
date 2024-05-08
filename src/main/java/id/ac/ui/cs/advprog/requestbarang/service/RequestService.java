46e7fe4507c637ca8f7d7471fd301a8e5b2e7abe
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-05-08 17:44:55 +0700

    [REFACTOR] change generate id method

--- src/main/java/id/ac/ui/cs/advprog/requestbarang/service/RequestService.java
+++ src/main/java/id/ac/ui/cs/advprog/requestbarang/service/RequestService.java
@@ -10,8 +10,7 @@ public class RequestService {
     public Request createAndProcessRequest(String name, String imageLink, double price, String currency,
             String storeLink) {
         int counter = 0;
-        String requestId = "R" + String.format("%03d", counter++);
-        Request request = RequestFactory.createRequest(requestId, name, price, imageLink, currency, storeLink);
+        Request request = RequestFactory.createRequest(name, imageLink, price, imageLink, currency, storeLink);
         return request;
     }
 }
