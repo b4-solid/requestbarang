3b2ed45b0dfbc7ecb11092ad5fb1a6a0d573e856
Nasya Ayu <ayu.siti21@ui.ac.id>
2024-05-10 19:40:18 +0700

    [REFACTOR] move getter setter, add annotation for constructor

--- src/main/java/id/ac/ui/cs/advprog/requestbarang/model/Request.java
+++ src/main/java/id/ac/ui/cs/advprog/requestbarang/model/Request.java
@@ -11,6 +11,9 @@ import id.ac.ui.cs.advprog.requestbarang.enums.RequestStatus;
 import lombok.Builder;
 
 @Entity
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "RequestBarang")
 public class Request {
 
@@ -18,58 +21,29 @@ public class Request {
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     @Column(name = "id")
     @NotNull
-    @Setter
-    @Getter
     private long id;
 
     @Column(name = "product_id")
-    @Setter
-    @Getter
     private long productId;
 
     @Column(name = "harga")
     @NotNull
-    @Setter
-    @Getter
     private int harga;
 
     @Column(name = "name")
     @NotNull
-    @Setter
-    @Getter
     private String name;
 
     @Column(name = "deskripsi")
-    @Setter
-    @Getter
     private String deskripsi;
 
     @Column(name = "image_link")
-    @Setter
-    @Getter
     private String imageLink;
 
     @Column(name = "store_link")
-    @Setter
-    @Getter
     private String storeLink;
 
     @Column(name = "status")
-    @Setter
-    @Getter
     private Boolean status;
-
-    public Request(long id, long productId, int harga, String name, String deskripsi, String imageLink, String storeLink,
-            Boolean status) {
-        this.id = id;
-        this.productId = productId;
-        this.harga=harga;
-        this.name = name;
-        this.deskripsi = deskripsi;
-        this.imageLink = imageLink;
-        this.storeLink = storeLink;
-        this.status = status;
-
-    }
 
 }
